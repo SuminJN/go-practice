efac995346b967d6d3e5e68c2eefdedb8907c8f6
Sumin Jeon <wjstnals1211@gmail.com>
2025-11-17 20:10:56 +0900

    feat: 포인터 연습

---
+++ pointers/pointers.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	i, j := 42, 2701
+	var p *int
+
+	p = &i          // i를 가리키는 포인터
+	fmt.Println(*p) // 포인터를 통해 i 값을 읽습니다.
+	*p = 21         // 포인터를 통해 i값을 설정합니다.
+	fmt.Println(i)
+
+	p = &j       // j를 가리킵니다.
+	*p = *p / 37 // 포인터를 통해 j를 나눕니다.
+	fmt.Println(j)
+}
